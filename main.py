aee3e7be5bc917e49d33a725ce8adcc6abb31f62
Michael Cummings <126340452+MDCummings86@users.noreply.github.com>
2023-04-07 00:01:45 -0400

    Add files via upload
    I wasn't able to git push from the terminal to include both folders. When I tried to git push and entered my credentials for the last step (getting them right from GitHub) the message back was failed and when I tried to upload the folders directly to GitHub it seemed to make my computer freeze. 
    
    I needed help from TA's this week with the loops. I had my gameplay for iterating over rows then appending to lists. I wasn't able to nail the formatting of the txt printouts. Also my delimiter is off on the second one and I couldn't figure out how to replace it for the csvwriter

--- main.py
+++ main.py
@@ -1,98 +1,97 @@
-# Modules
 import os
 import csv
 
-#input the file
-csv_path = "Resources/budget_data.csv"
+election_data = os.path.join('Resources/election_data.csv')
+# print("hello")
+# Open the CSV file
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-#read the file and make into a list
-with open(csv_path) as interim_financials:
-    reader = csv.reader(interim_financials)
+  # The total number of votes cast (skip header), assign to list
+    next(csvreader)
+    rows = list(csvreader)
+    total_votes = len(rows)
+    total_votes = total_votes
 
-    #establish indexes for month, income/loss and change
-    profit_loss = []
-    profit_loss_change = []
-    months = []
+  # Create new list from Candidate colum to count votes for each candidate iterating through all the rows
+  # used python range function 'LoopDeLoop.py'
+    candidate_names = list()
+    amount = list()
+    for active_rows in range (0,total_votes):
+        if active_rows == "Charles Casper Stockham":
+            print(active_rows)
+        candidate = rows[active_rows][2]
+        amount.append(candidate)
+        # if statement to determine how many candidates (assistance from TA)
+        if candidate not in candidate_names:
+            candidate_names.append(candidate)
+    number_of_candidates = len(candidate_names)
+    # print(number_of_candidates)
+    # print(candidate_names)
 
-    #Count the number of total rows, skipping the header
-    rows = len(list(csv_path))
-    next(reader, None)
-    # print(rows) 
+  # Append the lists to count and calculate total votes and percentage
+    votes = list()
+    percentage = list()
+    for columns in range (0,number_of_candidates):
+        name = candidate_names[columns]
+        votes.append(amount.count(name))
+        decimal = votes[columns]/total_votes
+        percentage.append(decimal)
+        # print(decimal)
+        # print(columns)
 
-    #subtract the header from the total rows
-    rows_minus_header = (rows-1)
+  # variable for the winner '1'
+    winner = votes.index(max(votes))
+    # print(winner)
 
-    #Calculate the sum and average of the profit/loss 
-    csvreader = csv.reader(interim_financials, delimiter= ',')
-    for row in csvreader:
-
-        profit_loss.append(float(row[1]))
-        months.append(row[0])
-
-        total_months = len(months)
-        total_revenue = sum(profit_loss)
-
-    #For loop to iterate through rows while iterating from one row to the next to calculate average
-    for row in range(1,len(profit_loss)):
-        profit_loss_change.append(profit_loss[row] - profit_loss[row-1])   
-        avg_revenue_change = sum(profit_loss_change)/len(profit_loss_change)
-        max_revenue_change = max(profit_loss_change)
-        min_revenue_change = min(profit_loss_change)
-        max_revenue_change_date = str(months[profit_loss_change.index(max(profit_loss_change))])
-        min_revenue_change_date = str(months[profit_loss_change.index(min(profit_loss_change))])
-        
-    #Display the Finanacial Analysis, Total Months, and Total Revenue
-    #line skip from https://stackoverflow.com/questions/31489377/working-of-n-in-python
+  # Print the results to terminal with percentage formatting 3 decimal places
     print("\n")
-    print("Financial Analysis")
-    print("---------------------------------")
-    print("Total Months:", total_months)
-    print("Total Revenue: $", total_revenue)
-    print("Average Revenue Change: $", round(avg_revenue_change))
-    print("Greatest Increase in Revenue:", max_revenue_change_date,"($", max_revenue_change,")")
-    print("Greatest Decrease in Revenue:", min_revenue_change_date,"($", min_revenue_change,")") 
+    print("Election Results")
+    print("----------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("----------------------------")
+    for z in range (0,number_of_candidates):
+        print(f"{candidate_names[z]}: {percentage[z]:} ({votes[z]:,})")
+    print("----------------------------")
+    print(f"Winner: {candidate_names[winner]}")
+    print("----------------------------")
     print("\n")
 
+# Write the results to "election_data_output.txt" file
 # Specify the file to write to
-output_path = os.path.join("budget_data_output.txt")
+output_path = os.path.join("Analysis/election_data_output.txt")
 
-# Open the file using "write" mode. Specify the variable to hold the contents
+# Open the file using "write" mode. 
 with open(output_path, 'w') as txtfile:
 
-    # Initialize csv.writer
+    # involve the csv.writer
     csvwriter = csv.writer(txtfile, delimiter=' ')
 
 # Write the header
-    csvwriter.writerow(["Financial Analysis"])
+    csvwriter.writerow(['Election Results'])
 
     # Header Separator
     csvwriter.writerow(['----------------------------------'])
     
-    # Total Months row
-    csvwriter.writerow(['Total Months:',total_months])
+    # Total Votes row
+    csvwriter.writerow(['Total Votes:',total_votes])
 
-    # Total Revenue row
-    csvwriter.writerow(['Total Revenue:',total_revenue])
+    # Separator
+    csvwriter.writerow(['----------------------------------'])
 
-    #Average Revenue Change row 
-    csvwriter.writerow(['Average Revenue Change:',avg_revenue_change])
+    #Votes for Candidate
+    for z in range (0,number_of_candidates):
+        csvwriter.writerow(f"{candidate_names[z]}: {percentage[z]:.3%} ({votes[z]:,})")
+    
+    # Separator
+    csvwriter.writerow(['----------------------------------']) 
 
-    #Greatest Increase in Revenue row
-    csvwriter.writerow(['Greatest Increase in Revenue:',max_revenue_change_date,max_revenue_change])
+     # Winner
+    csvwriter.writerow([f"Winner: {candidate_names[winner]}"])
 
-    #Greatest Decrease in Revenue row
-    csvwriter.writerow(['Greatest Decrease in Revenue:', min_revenue_change_date,min_revenue_change])
- 
-
- 
-
-
-
-
+    # Separator
+    csvwriter.writerow(['----------------------------------'])
     
 
 
-
-
-   
     
